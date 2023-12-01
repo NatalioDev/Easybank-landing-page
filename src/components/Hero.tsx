2fcd8d550474fb419fdfc649aff625bc97abeb5e
NatalioDev <nataliodg.dev@gmail.com>
2023-12-01 11:16:48 -0300

    add styles mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,9 @@
 import { useEffect } from "react";
 import "./styles/hero.css"
 import { motion, useAnimation, AnimatePresence } from 'framer-motion';
-// Importa la librería Styled Components
-import styled from 'styled-components';
 // import bgImage from 'images/bg-intro-desktop.svg';
 
 
-//Creo estos estilos aca para resolver el problema de rutas de github pages
-    const Container = styled.div `
-    background: url(images/bg-intro-desktop.svg) , var(--very-light-gray),
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    background-repeat: no-repeat;
-    background-position-x: 43.4vw;
-    background-position-y: -260px;
-  
-    @media screen and (max-width: 768px) {
-      background: url(images/bg-intro-mobile.svg), var(--very-light-gray);
-      background-repeat: no-repeat;
-      background-position-x: right;
-      background-position-y: -2rem;
-    }
-    `;
 
 export default function Hero() {
     const controls = useAnimation();
@@ -69,42 +50,39 @@ export default function Hero() {
     },[controls, controlsTitle, controlsDescription, controlsButton]);
 
   return (
-    <Container>
-        <div className="hero_wrapper">
-            <AnimatePresence>
-                <section className="hero_info">
-                    <motion.h1
-                        initial={{y:-100}}
-                        animate={controlsTitle}
-                        className="hero_info_title"
-                    >Next generation digital banking
-                    </motion.h1>
-                    <motion.p 
-                        className="hero_info_description "
-                        initial={{y:100}}
-                        animate={controlsDescription}
-                    >
-                    Take your financial life online. Your Easybank account will be a one-stop-shop
-                    for spending, saving, budgeting, investing, and much more.
-                    </motion.p>
-                    <motion.div 
-                        className="hero_btn"
-                        initial={{y:100, opacity: 0}}
-                        animate={controlsButton}
-                    >
-                        <a href="#" className="hero_request_btn">Request invite</a>
-                    </motion.div>
-                </section>
-                <motion.div 
-                    className="hero_image"
-                    initial={{y:-150, opacity: 0 }}
-                    animate={controls}
-                >
-                    <figure>
-                        <img src="images/image-mockups.png" alt="pattern image" className="hero_image_img" />
-                    </figure>
-                </motion.div>
-            </AnimatePresence>
-        </div>
-    </Container >
+        <section className="hero">
+            <div className="hero_wrapper">
+                <AnimatePresence>
+                        <motion.h1
+                            initial={{y:-100}}
+                            animate={controlsTitle}
+                            className="hero_info_title"
+                        >Next generation digital banking
+                        </motion.h1>
+                        <motion.p 
+                            className="hero_info_description "
+                            initial={{y:100}}
+                            animate={controlsDescription}
+                        >
+                        Take your financial life online. Your Easybank account will be a one-stop-shop
+                        for spending, saving, budgeting, investing, and much more.
+                        </motion.p>
+                        <motion.div 
+                            className="hero_btn"
+                            initial={{y:100, opacity: 0}}
+                            animate={controlsButton}
+                        >
+                            <a href="#" className="hero_request_btn">Request invite</a>
+                        </motion.div>
+                            <img src="images/bg-intro-desktop.svg" alt="" className="hero_bg" />
+                            <motion.img 
+                                src="images/image-mockups.png" 
+                                alt="pattern image" 
+                                className="hero_image"
+                                initial={{y:-150, opacity: 0 }}
+                                animate={controls} 
+                            />
+                </AnimatePresence>
+            </div>
+        </section>
 )}
