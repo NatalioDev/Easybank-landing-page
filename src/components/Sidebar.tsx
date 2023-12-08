43282458e81345d3d1cf34f61569c4aa331e7b87
NatalioDev <nataliodg.dev@gmail.com>
2023-12-07 22:36:08 -0300

    add styles mobile hamburg

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -35,11 +35,11 @@ export default function Sidebar({ Open = false, setOpen }: Props) {
                         <img src="images/xmark.svg" alt="Hamburg" className="sidebar_img" />
                     </button>
                     <div className="sidebar_content">
-                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav link_hover ">Home</a></li>
-                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav link_hover ">About</a></li>
-                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav link_hover ">Contact</a></li>
-                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav link_hover ">Blog</a></li>
-                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav link_hover ">Careers</a></li>
+                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav  ">Home</a></li>
+                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav  ">About</a></li>
+                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav  ">Contact</a></li>
+                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav  ">Blog</a></li>
+                        <li className="sidebar_menu_item_nav "><a href="#" className="sidebar_menu_item_link_nav  ">Careers</a></li>
                         <a href="#" className="sidebar_request_btn">Request Invite</a>
                     
                     </div>
